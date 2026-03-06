b6cd9ca9c8dcd12a5d86f259df6482939a17c808
CoderDevOp <lingwaldevanshu@gmail.com>
2026-03-06 15:17:26 +0530

    UC 10

---
+++ App/src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Normalize string (remove spaces and convert to lowercase)
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while(start < end) {
+            if(normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if(isPalindrome)
+            System.out.println("Palindrome (Ignoring spaces and case)");
+        else
+            System.out.println("Not a Palindrome");
+
+        sc.close();
+    }
+}
